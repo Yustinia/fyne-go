015bf15583cebf42ea43eced507866657f0c58c1
Yustinia <yustinia@protonmail.com>
2026-08-12 19:08:38 +0800

    initialization

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Fyne Practice";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          go # runtime
+          gopls # lsp
+          golangci-lint # lint
+          gofumpt # fmt
+
+          # fyne components
+          libGL
+          pkg-config
+          xorg.libX11.dev
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXrandr
+          xorg.libXxf86vm
+          libxkbcommon
+          wayland
+
+          #make
+          gnumake
+          checkmake
+          mbake
+        ];
+      };
+    };
+}
